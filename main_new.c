67947b52bc46d9c0967a23394462114c695427d3
Lechun Xing <lxing@cita.utoronto.ca>
2020-05-06 17:12:58 -0400

    update files main_new.c Makefile outputfile

---
+++ main_new.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "average.h"
+
+/*this is a comment.*/
+
+
+int main() {
+    double arr[] = {1.0, 2.0, 3.0, 4.0};
+
+    double result = average(4, arr);
+
+    printf("The average of 1, 2, 3 and 4 is: %.4f\n", result);
+    return 0;    
+}
+
